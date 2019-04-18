0ef34f09ed8e1da1ea58b646a31957920e77ac98
Bo Zhao <boz5@illinois.edu>
2019-04-18 17:00:59 -0500

    create superuser

--- shareLifeWeb/settings.py
+++ shareLifeWeb/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'shareLifeWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join(BASE_DIR, 'db.db'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
